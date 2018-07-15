6900bef4e0df6320516b33bbf8b20e5eaf0c86ac
Lucy Weatherley <lucyweath@msn.com>
2018-07-15 18:13:34 +0100

    Text wrapper added with padding to restaurant 'cards'

--- js/main.js
+++ js/main.js
@@ -164,24 +164,30 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  // Create text wrapper
+  const textWrapper = document.createElement('div');
+  textWrapper.className = 'text-wrapper';
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textWrapper.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textWrapper.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  textWrapper.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  textWrapper.append(more);
 
-  return li
+  li.append(textWrapper);
+
+  return li;
 }
 
 /**
