b3a6aa2d784c6722fc770a42e0a867e65acee773
Lucy Weatherley <lucyweath@msn.com>
2018-07-15 18:05:58 +0100

    Flex box grid system for restaurant 'cards'

--- js/main.js
+++ js/main.js
@@ -157,6 +157,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.className = 'col'; // Added 'col' class
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
