a8963d406573b0edebe69d1b129cce44edaf92f4
Lucy Weatherley <lucyweath@msn.com>
2018-07-18 22:30:24 +0100

    Image alt tags added

--- js/main.js
+++ js/main.js
@@ -167,6 +167,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = 'Photo of the restaurant, ' + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
